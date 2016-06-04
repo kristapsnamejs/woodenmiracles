c2a0be18a4cd79878f73c581b17b8eef9fd6d10e
Kristaps Neimanis <Kristaps Neimanis>
2016-06-04 16:53:34 +0300

    angular...

---
+++ js/header.js
@@ -0,0 +1,11 @@
+//creating a new directive,
+//directive method takes two parameters:
+//name and array with dependencies and a function
+//that returns a directive object 
+angular.module('woodApp')
+    .directive('woodHeader', [function () {           
+        return {                                               
+            templateUrl: 'templates/header.html'          
+        };                                                  
+    }]);
+    
